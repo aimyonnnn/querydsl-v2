4c596abe37bc9f0bb78ab9f776935e0a92e43d45
SUMIN <lovepoemlovepoem0516@gmail.com>
2024-01-31 23:01:33 +0900

    feat: querydsl 기본 문법

--- src/test/java/com/example/Querydsl2ApplicationTests.java
+++ src/test/java/com/example/Querydsl2ApplicationTests.java
@@ -1,23 +1,55 @@
 package com.example;
 
-import com.example.entitiy.Hello;
-import com.example.entitiy.QHello;
+import com.example.entitiy.*;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static com.example.entitiy.QMember.*;
+import static com.example.entitiy.QTeam.team;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class Querydsl2ApplicationTests {
 
-    @Autowired
-    EntityManager em;
+    @Autowired EntityManager em;
+    JPAQueryFactory queryFactory;
+
+    /**
+     * 테스트 데이터 생성
+     */
+    @BeforeEach
+    public void before() {
+
+        queryFactory = new JPAQueryFactory(em); // 쿼리팩토리 생성
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
 
     @Test
     void contextLoads() {
@@ -36,6 +68,105 @@ class Querydsl2ApplicationTests {
         assertThat(result.getId()).isEqualTo(hello.getId());
 
     }
+
+    /**
+     * 팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: select m, t from Member m left join m.team t on t.name = 'teamA'
+     */
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 페치 조인
+     */
+    @PersistenceUnit EntityManagerFactory emf;
+    @Test // 페치 조인 미적용
+    public void fetchJoinNo() {
+        em.flush(); // 영속성 컨텍스트에 있는 쿼리를 DB에 반영
+        em.clear(); // 영속성 컨텍스트를 초기화
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 미적용").isFalse();
+    }
+
+    @Test // 페치 조인 적용
+    public void fetchJoinUse() {
+        em.flush();
+        em.clear();
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 적용").isTrue();
+    }
+
+    /**
+     * 서브쿼리
+     * 나이가 가장 많은 회원 조회
+     */
+    @Test
+    public void subQuery() {
+        // 서브쿼리의 별칭을 지정해주어야 한다.
+        QMember memberSub = new QMember("memberSub");
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(
+                        JPAExpressions
+                                .select(memberSub.age.max())
+                                .from(memberSub)
+                ))
+                .fetch();
+        assertThat(result).extracting("age")
+                .containsExactly(40);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
